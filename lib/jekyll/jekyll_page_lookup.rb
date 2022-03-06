5c8cf3e64b7e1d9bb43e048a1868d5b56cb71958
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-05 19:09:33 -0500

    -

--- lib/jekyll/jekyll_page_lookup.rb
+++ lib/jekyll/jekyll_page_lookup.rb
@@ -10,50 +10,47 @@ module Jekyll
 
     # Main plugin action, called by Jekyll-core
     def generate(site)
-      @site = site
-      @site.pages << page_lookup unless file_exists?(page_lookup_txt)
-      Jekyll.logger "#{@site.pages.length} pages were found."
+      pages = assemble_pages(site)
+      File.open(page_lookup_txt, 'w') do |file|
+        pages.each do |page|
+          # Jekyll.logger.info "#{page.name} path: #{page.relative_path} URL path: #{page.url}"
+          Jekyll.logger.info "#{page.name} is a #{type(page)}"
+          file.puts "#{page.relative_path} #{page.url}" if page.ext == '.html' && type(pages[0]) == Jekyll::StaticFile
+        end
+      end
     end
 
     private
 
-    INCLUDED_EXTENSIONS = %w[
-      .md
-      .htm
-      .html
-      .xhtml
-    ].freeze
+    def interesting_page(page)
+      ok = false
+      ok = page.html? if page.class.method_defined? :html?
+      ok &&= page.base == @site.source if page.class.method_defined? :base
+      ok
+    end
 
-    # Matches all whitespace that follows
-    #   1. A '>' followed by a newline or
-    #   2. A '}' which closes a Liquid tag
-    # We will strip all of this whitespace to minify the template
-    MINIFY_REGEX = /!(?<=>\n|})\s+/.freeze
+    def assemble_pages(site)
+      @site = site
+      pages = site.pages.select { |page| interesting_page page }
+      site.collections.each do |collection|
+        docs = collection[1].docs
+        interesting_files = docs.select { |doc| interesting_page doc }
+        pages |= [interesting_files] unless interesting_files.empty?
+      end
+      Jekyll.logger.info "#{pages.length} pages were found in #{@site.source}."
+      pages
+    end
 
     def page_lookup_txt
       "#{@site.source}/_page_lookup.txt"
     end
 
-    # Array of all non-jekyll site files with an HTML extension
-    def static_files
-      @site.static_files.select { |file| INCLUDED_EXTENSIONS.include? file.extname }
-    end
-
     def page_lookup
-      output = PageWithoutAFile.new(@site, __dir__, '', '_page_lookup.txt')
-      output.content = File.read(@site.source).gsub(MINIFY_REGEX, '')
-      # output.data["layout"] = nil
-      # output.data["static_files"] = static_files.map(&:to_liquid)
+      output = PageWithoutAFile.new(@site, @site.source, '', '_page_lookup.txt')
+      output.content = File.exist?(page_lookup_txt) ? File.read(page_lookup_txt) : ''
+      output.data["layout"] = nil
+      output.data["static_files"] = static_files.map(&:to_liquid)
       output
-    end
-
-    # Checks if a file already exists in the site source
-    def file_exists?(file_path)
-      pages_and_files.any? { |p| p.url == "/#{file_path}" }
-    end
-
-    def pages_and_files
-      @pages_and_files ||= @site.pages + @site.static_files
     end
   end
 end
